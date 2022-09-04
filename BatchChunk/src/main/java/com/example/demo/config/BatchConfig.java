bc5bb4d84b41c7621bdf3b4d5e65aa9c11bc3368
got.hideo <got.hideo@gmail.com>
2022-09-05 01:13:14 +0900

    chunk用のjob、step作成

---
+++ BatchChunk/src/main/java/com/example/demo/config/BatchConfig.java
@@ -0,0 +1,46 @@
+package com.example.demo.config;
+
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig {
+	
+	@Autowired
+	private JobBuilderFactory jobBuilderFactory;
+	
+	@Autowired
+	private StepBuilderFactory stepBuilderFactory;
+	
+	@Autowired
+	private ItemReader<String> reader;
+	
+	@Autowired
+	private ItemProcessor<String,String> processor;
+	
+	@Autowired
+	private ItemWriter<String> writer;
+	
+	@Bean
+	public Step chunkStep() {
+		return stepBuilderFactory.get("HelloChunStep").<String,String>chunk(1).reader(reader).processor(processor).writer(writer).build();
+	}
+	
+	@Bean
+	public Job chunkJob() throws Exception{
+		return jobBuilderFactory.get("HelloWorldChunkJob").incrementer(new RunIdIncrementer()).start(chunkStep()).build();
+	}
+
+}
